10baeeee2e73ac4e65fd4b04eca6165dd1c86af5
floalt <53464175+floalt@users.noreply.github.com>
2020-03-09 15:39:44 +0100

    ver 0.61
    install-nextcloud.ps1 is a script for installing and/or updateing
    this is then new cool shit!
    the scripts looks for the most recent installation file and decides what to do with it

---
+++ install-nextcloud.ps1
@@ -0,0 +1,55 @@
+﻿# description: Install Nextcloud Client
+# author: flo.alt@fa-netz.de
+# version: 0.61
+
+# bevore runnin this script: download nextcloud setup file and store it in a path this script can access ($deploypath).
+
+# setup here:
+$deploypath = "\\serv12-dc\deployment\nextcloud\"
+$logpath = "\\serv12-dc\deployment\logs\nextcloud\"
+$logfile =  $logpath + $env:COMPUTERNAME + ".log"
+$logokfile = $logpath + "ok_" + $env:COMPUTERNAME + ".log"
+$logfailfile = $logpath + "fail_" + $env:COMPUTERNAME + ".log"
+
+# look for most recent setup file:
+$setupfileinfo = Get-ChildItem -File $deploypath | Sort-Object VersionInfo | select -Last 1
+$filename = $setupfileinfo.name
+$version = $setupfileinfo.VersionInfo.FileVersion
+$setup = $deploypath + $filename
+
+
+# logfile entry:
+(Get-Date -Format yyyy-MM-dd-HH:mm:ss) + " START logfile`n" >> $logfile
+"try to install Nextcloud $version" >>  $logfile
+"from $setup" >>  $logfile
+
+$check_nc = Get-Package -Provider Programs | where {$_.Name -like "Nextcloud*"}
+
+# if Nexcloud is not installed yet
+if (!$check_nc) {
+    (Get-Date -Format HH:mm:ss) + " Nextcloud is not installed. Installing Nexcloud Client now." >> $logfile
+    & $setup "/S" *>> $logfile
+
+#    # check if Installation went all right
+#    if ($check_nc.Version -eq $version) {
+#        (Get-Date -Format HH:mm:ss) + " Installation of Nexcloud Client $version DONE. Exiting" >> $logfile
+#        (Get-Date -Format yyyy-MM-dd-HH:mm:ss) + " Nextcloud Client $version is installed." > $logokfile
+#    } else {
+#        (Get-Date -Format HH:mm:ss) + " Installation of Nexcloud Client $version FAILED. Exiting" >> $logfile
+#        (Get-Date -Format yyyy-MM-dd-HH:mm:ss) + " Installation of Nexcloud Client $version FAILED." > $logfailfile
+#    }
+} else {
+    echo $check_nc
+    # if Nextcloud is already installed, but older version
+    if ($check_nc.Version -lt $version) {
+        (Get-Date -Format HH:mm:ss) + " Installed Version is less than $version. Running for Update" >> $logfile
+        & $setup "/S" *>> $logfile
+    # if Nextcloud is already installed and up to date
+    } else {
+        (Get-Date -Format HH:mm:ss) + " Installed Version is up to date. Exiting" >> $logfile
+        (Get-Date -Format yyyy-MM-dd-HH:mm:ss) + " Nextcloud Client $version is installed." > $logokfile
+    }
+}
+
+"`n" + (Get-Date -Format yyyy-MM-dd-HH:mm:ss) + " END logfile" >> $logfile
+"`n===================================== `n" >> $logfile
